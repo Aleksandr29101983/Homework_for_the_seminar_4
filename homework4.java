06ed7a16992c0cfac033a545c614af7df9f3d4e6
Aleksandr <kossander@bk.ru>
2023-02-27 19:18:58 +0300

    A method of ranking 'parents' and 'children' has been created

--- homework4.java
+++ homework4.java
@@ -31,4 +31,26 @@ public class homework4 {
 
 
     }
+
+    private static void heapify(int[] arr, int i, int n) { // Ранжирование "родителей" и "детей"
+        int l = i * 2 + 1;
+        int r = i * 2 + 2;
+        int largest = i;
+
+        if(l < n && arr[l] > arr[largest]) {
+            largest = l;
+        }
+
+        if(r < n && arr[r] > arr[largest]) {
+            largest = r;
+        }
+
+        if(i != largest) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+
+            heapify(arr, largest, n);
+        }
+    }
 }
