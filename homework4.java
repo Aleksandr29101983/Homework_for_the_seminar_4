5fd4130b00c8ddcc8c3351e85a282d8cfa6ec09a
Aleksandr <kossander@bk.ru>
2023-02-27 19:13:00 +0300

    A method for reading from a file has been created

---
+++ homework4.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * homework4
+ */
+public class homework4 {
+    public static void main(String[] args) {
+       
+    }
+
+    private static int[] readFile() throws FileNotFoundException {
+        File file = new File("input.txt");
+        Scanner scanner = new Scanner(file);
+        String[] stringArray = scanner.nextLine().split(",");
+        int[] intArray = new int[stringArray.length];
+
+        for(int i = 0; i < stringArray.length; i++){
+            intArray[i] = Integer.parseInt(stringArray[i]);
+        }
+        scanner.close();
+        return intArray;
+    }
+}
